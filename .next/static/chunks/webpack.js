301421189369462e2b3f789a855b0e55daebb750
gweissend <why.grey.matter@gmail.com>
2025-04-01 17:36:17 -0400

    syntax updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "810a7aa5e5e823d1"; }
+/******/ 		__webpack_require__.h = function() { return "a38d6d531b6e7751"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
